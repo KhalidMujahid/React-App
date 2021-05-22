583df6e6f0fcb4019bd4e4bcba152e287d77b552
khalidMujahid <binkhalid267@gmail.com>
2021-05-23 00:38:14 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component{
+  render(){
+  	return(
+       <>
+          <h1>Hello world</h1>
+          <p>Goto src/App.js to edit this page</p>
+       </>
+  	)
+  }
+}
+
+export default App;
